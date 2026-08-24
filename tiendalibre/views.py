4ecd444fd5d5164bbaeea2085b6f2d1f96aadf8e
LucciMalap <luccianomalap@gmail.com>
2026-08-24 11:35:27 -0300

    retoque

--- tiendalibre/views.py
+++ tiendalibre/views.py
@@ -2,10 +2,8 @@ from django.shortcuts import render
 from .models import Producto, Categoria
 
 def catalogo_productos(request):
-    # Obtenemos todas las categorías para mostrar los filtros
     categorias = Categoria.objects.all()
     
-    # Revisamos si la URL trae un parámetro de filtro (ej: ?categoria=1)
     categoria_id = request.GET.get('categoria')
     
     if categoria_id:
